8aadbcb70311eab4e7d57deb8fb65cba6ae261dd
Shai Givati <shai@gmail.com>
2023-06-17 16:07:23 +0300

    general

--- src/main/java/ApiUtil.java
+++ src/main/java/ApiUtil.java
@@ -12,15 +12,23 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ApiUtil {
+
+    static {
+        System.out.println("Enter user id: ");
+        ApiUtil.userId = new Scanner(System.in).nextLine();
+    }
+    private static String userId;
+
     public static void send(List<River> riverList) throws Exception {
         try {
             Gson gson = new Gson();
             String json = gson.toJson(riverList);
 
             // The rest is the same as before
-            URL url = new URL("http://localhost:9030/fm1/send-data");
+            URL url = new URL("http://localhost:9030/fm1/send-data?userId=" + ApiUtil.userId);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json");
@@ -51,6 +59,19 @@ public class ApiUtil {
                         .limit(printSize)
                         .forEach(river -> System.out.println(river.getName()));
                 System.out.println("and " + (riversResponseModel.getRiverList().size() - printSize) + " more");
+                if (riversResponseModel.getMatched() >= 50) {
+                    System.out.println("Would you like to continue to the next task? 1 - for YES, 2 - for NO ");
+                    Scanner scanner = new Scanner(System.in);;
+                    String text = scanner.nextLine().trim();
+                    if (text.equals(String.valueOf(1))) {
+                    } else if (text.equals(String.valueOf(2))) {
+                        System.out.println("Ok, try again");
+                        System.exit(1);
+                    } else {
+                        System.out.println("Invalid answer!");
+                    }
+
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -59,6 +80,7 @@ public class ApiUtil {
         }
 
     }
+
 
     public static JSONObject sendApiGetRequest(String path) {
         return sendApiGetRequest(path, new HashMap<>());
@@ -95,5 +117,9 @@ public class ApiUtil {
             ex.printStackTrace();
         }
         return jsonObject;
+    }
+
+    public static void setUserId(String userId) {
+        ApiUtil.userId = userId;
     }
 }
